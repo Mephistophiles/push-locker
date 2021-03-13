0b53e370239710057269d687d64e345510563e40
Maxim Zhukov <mussitantesmortem@gmail.com>
2021-03-13 16:20:25 +0300

    add lightweight cli for check lock state

---
+++ pushlock-check/src/main.rs
@@ -0,0 +1,16 @@
+// use reqwest::StatusCode;
+use hyper::{Client, StatusCode, Uri};
+use std::{env, process, str::FromStr};
+
+#[tokio::main]
+async fn main() {
+    let server = env::args().skip(1).next().expect("Server address");
+    let endpoint = format!("http://{}/is_locked", server);
+    let client = Client::new();
+    let res = client.get(Uri::from_str(&endpoint).unwrap()).await.unwrap();
+
+    match res.status() {
+        StatusCode::LOCKED => process::exit(1),
+        _ => process::exit(0),
+    }
+}
