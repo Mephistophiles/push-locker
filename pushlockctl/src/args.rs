bcd2b21de4c9f001dafef625c0df59336cccad85
Maxim Zhukov <mussitantesmortem@gmail.com>
2021-03-13 20:20:31 +0300

    Prettify output and support config for pushlockctl

--- pushlockctl/src/args.rs
+++ pushlockctl/src/args.rs
@@ -1,10 +1,15 @@
-use std::{io, process};
-
-use clap::{App, Arg, ArgMatches};
+use clap::{App, AppSettings, Arg, ArgMatches};
 use clap_generate::{
     generate,
     generators::{Bash, Elvish, Fish, PowerShell, Zsh},
 };
+use indoc::indoc;
+use serde::Deserialize;
+use std::{
+    fs, io,
+    process::{self, abort},
+};
+use user_error::{UserFacingError, UFE};
 
 pub(crate) enum Action {
     Lock,
@@ -12,8 +17,18 @@ pub(crate) enum Action {
     CheckState,
 }
 
+impl Default for Action {
+    fn default() -> Self {
+        Action::Lock
+    }
+}
+
+#[derive(Deserialize)]
 pub(crate) struct Config {
+    pub(crate) server: String,
+    #[serde(skip)]
     pub(crate) action: Action,
+    #[serde(skip)]
     pub(crate) endpoint: String,
 }
 
@@ -32,11 +47,24 @@ fn autocomplete(shell: &str) {
 
 fn app() -> App<'static> {
     App::new("pushlock")
-        .subcommand(App::new("generate").arg(Arg::new("shell").takes_value(true).required(true)))
-        .subcommand(App::new("lock"))
-        .subcommand(App::new("unlock"))
-        .subcommand(App::new("check"))
-        .arg(Arg::new("server").takes_value(true).required(true))
+        .author(clap::crate_authors!())
+        .about(clap::crate_description!())
+        .version(clap::crate_version!())
+        .setting(AppSettings::SubcommandRequiredElseHelp)
+        .subcommand(
+            App::new("generate")
+                .about("generate autocomplete for shell")
+                .arg(Arg::new("shell").takes_value(true).required(true)),
+        )
+        .subcommand(App::new("lock").about("Try to reserve a push window"))
+        .subcommand(App::new("unlock").about("Release push window"))
+        .subcommand(App::new("check").about("Check for current push window"))
+        .arg(
+            Arg::new("server")
+                .long("server")
+                .takes_value(true)
+                .about("Specify server address"),
+        )
 }
 
 fn args() -> ArgMatches {
@@ -44,7 +72,35 @@ fn args() -> ArgMatches {
 }
 
 impl Config {
-    pub(crate) fn from_args() -> Self {
+    pub(crate) fn from_config() -> Self {
+        let mut home_dir = dirs::config_dir().unwrap();
+        home_dir.push("pushlock");
+
+        fs::create_dir_all(&home_dir).unwrap();
+
+        home_dir.push("config.toml");
+
+        let f = fs::read_to_string(&home_dir).unwrap_or_else(|_| {
+            UserFacingError::new("Unable to open config file")
+                .reason("File not found")
+                .help(format!(
+                    indoc! {"
+                    Please create file {} with content:
+
+                    server = <pushlock server>
+                    "},
+                    home_dir.display()
+                ))
+                .print_and_exit();
+            abort();
+        });
+
+        let mut c: Config = toml::from_str(&f).unwrap();
+        c.from_args();
+        c
+    }
+
+    fn from_args(&mut self) {
         let args = args();
 
         if let Some(generate) = args.subcommand_matches("generate") {
@@ -52,7 +108,7 @@ impl Config {
             process::exit(0);
         }
 
-        let action = if args.is_present("lock") {
+        self.action = if args.is_present("lock") {
             Action::Lock
         } else if args.is_present("unlock") {
             Action::Release
@@ -62,14 +118,14 @@ impl Config {
             panic!("Invalid action");
         };
 
-        let ip = args.value_of("server").expect("server");
+        if let Some(server) = args.value_of("server") {
+            self.server = server.to_string();
+        }
 
-        let endpoint = match action {
-            Action::Lock => format!("http://{}/lock", ip),
-            Action::Release => format!("http://{}/release", ip),
-            Action::CheckState => format!("http://{}/is_locked", ip),
+        self.endpoint = match self.action {
+            Action::Lock => format!("http://{}/lock", self.server),
+            Action::Release => format!("http://{}/release", self.server),
+            Action::CheckState => format!("http://{}/is_locked", self.server),
         };
-
-        Config { action, endpoint }
     }
 }
--- pushlockctl/src/main.rs
+++ pushlockctl/src/main.rs
@@ -1,42 +1,75 @@
 use args::{Action, Config};
-use reqwest::StatusCode;
-use std::process;
+use colored::*;
+use hyper::{Body, Client, Method, Request, Response, StatusCode};
+use serde::Deserialize;
+use std::{net::IpAddr, process};
 
 mod args;
 
+#[derive(Deserialize)]
+struct Locked {
+    locked_by: Vec<IpAddr>,
+}
+
+fn print_ok() {
+    println!("{}", "OK".green());
+}
+
+fn print_unknown_error() {
+    println!("{}", "Unknown error".red());
+}
+
+fn print_available() {
+    println!("{}", "Push available".green());
+}
+
+async fn print_locked(res: Response<Body>) {
+    let buf = hyper::body::to_bytes(res).await.expect("body");
+    let result: Locked = serde_json::from_slice(&buf).expect("json");
+
+    println!("{}", "Locked".yellow());
+
+    for ip in result.locked_by {
+        println!("* {}", ip);
+    }
+}
+
 #[tokio::main]
 async fn main() {
-    let args = Config::from_args();
+    let args = Config::from_config();
+    let client = Client::new();
 
-    let client = reqwest::Client::new();
+    let method = match args.action {
+        Action::Lock | Action::Release => Method::POST,
+        Action::CheckState => Method::GET,
+    };
+
+    let req = Request::builder()
+        .uri(&args.endpoint)
+        .method(method)
+        .body(Body::empty())
+        .unwrap();
+    let res = client.request(req).await.unwrap();
 
     match args.action {
-        Action::Lock | Action::Release => {
-            let res = client.post(&args.endpoint).send().await.unwrap();
-
-            match res.status() {
-                StatusCode::OK => println!("done"),
-                StatusCode::LOCKED => println!("already locked by: {}", res.text().await.unwrap()),
-                _ => println!("unknown error"),
+        Action::Lock | Action::Release => match res.status() {
+            StatusCode::OK => print_ok(),
+            StatusCode::LOCKED => print_locked(res).await,
+            _ => print_unknown_error(),
+        },
+        Action::CheckState => match res.status() {
+            StatusCode::OK => {
+                print_available();
+                process::exit(0);
             }
-        }
-        Action::CheckState => {
-            let res = client.get(&args.endpoint).send().await.unwrap();
-
-            match res.status() {
-                StatusCode::OK => {
-                    println!("unlocked");
-                    process::exit(0);
-                }
-                StatusCode::LOCKED => {
-                    println!("locked by: {}", res.text().await.unwrap());
-                    process::exit(1);
-                }
-                _ => {
-                    println!("unknown error");
-                    process::exit(0)
-                }
+            StatusCode::LOCKED => {
+                print_locked(res).await;
+                process::exit(1);
             }
-        }
+            _ => {
+                print_unknown_error();
+                process::exit(2)
+            }
+        },
     }
 }
