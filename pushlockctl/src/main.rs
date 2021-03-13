e08eb3124e49f2e1d77e907474b29211e045ada9
Maxim Zhukov <mussitantesmortem@gmail.com>
2021-03-13 16:19:33 +0300

    fix typo: req -> res

--- pushlockctl/src/main.rs
+++ pushlockctl/src/main.rs
@@ -12,24 +12,24 @@ async fn main() {
 
     match args.action {
         Action::Lock | Action::Release => {
-            let req = client.post(&args.endpoint).send().await.unwrap();
+            let res = client.post(&args.endpoint).send().await.unwrap();
 
-            match req.status() {
+            match res.status() {
                 StatusCode::OK => println!("done"),
-                StatusCode::LOCKED => println!("already locked by: {}", req.text().await.unwrap()),
+                StatusCode::LOCKED => println!("already locked by: {}", res.text().await.unwrap()),
                 _ => println!("unknown error"),
             }
         }
         Action::CheckState => {
-            let req = client.get(&args.endpoint).send().await.unwrap();
+            let res = client.get(&args.endpoint).send().await.unwrap();
 
-            match req.status() {
+            match res.status() {
                 StatusCode::OK => {
                     println!("unlocked");
                     process::exit(0);
                 }
                 StatusCode::LOCKED => {
-                    println!("locked by: {}", req.text().await.unwrap());
+                    println!("locked by: {}", res.text().await.unwrap());
                     process::exit(1);
                 }
                 _ => {
