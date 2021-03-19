050d97d824f5469c3fb4b5174640cbd1b670e521
Maxim Zhukov <mussitantesmortem@gmail.com>
2021-03-19 11:25:46 +0300

    Bump dist

--- dist/default.nix
+++ dist/default.nix
@@ -2,16 +2,16 @@
 
 rustPlatform.buildRustPackage rec {
   pname = "push-locker";
-  version = "0.1.1";
+  version = "0.1.3";
 
   src = pkgs.fetchFromGitHub {
     owner = "Mephistophiles";
     repo = pname;
     rev = "v${version}";
-    sha256 = "0r3ajiff0hd8zhdapi8vw559bnys48dr5wj4vgc8y9p56di3ggcb";
+    sha256 = "09bdjmm2rv0bivgl98f36625b8sa4sjmi4ll066vvlqw7wz9s701";
   };
 
-  cargoSha256 = "0si8j5j9sxf4y332byi3rs7l0bxl9ix2pjxr5rwh1mxdrl53y18d";
+  cargoSha256 = "0hdsvgjmdfqb0cwn3flv9qwirx88k0cqz1fh2nvwmx5zvj83fnz7";
 
   nativeBuildInputs = [ pkg-config ];
   buildInputs = [];
